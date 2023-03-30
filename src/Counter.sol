f9bf4ec98b262da25c44f290740528d6a0ed3cc9
Michał Konopka <konopek68@gmail.com>
2023-03-30 16:59:50 +0200

    bla bla

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "redstone-finance/data-services/TwapsDemoConsumerBase.sol";
+
 contract Counter {
     uint256 public number;
 
@@ -9,6 +11,6 @@ contract Counter {
     }
 
     function increment() public {
-        number++;
+        number += 2;
     }
 }
