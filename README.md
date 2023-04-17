78b0c1ac4f3fbd521dba6a3a8dbe72963bd648d7
Michał Konopka <konopek68@gmail.com>
2023-04-17 14:52:07 +0200

    blabla

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Foundry <> Redstone
 
 1. Clone
-   `git clone --recurse-submodules -j8`
+   `git clone --recurse-submodules -j8 https://github.com/redstone-finance/minimal-foundry-repo.git`
 2. Build redstone protocol
-   `cd ./lib/redstone-oracles-monorepo/packages/protocol && yarn build`
+   `cd ./lib/redstone-oracles-monorepo/packages/protocol && yarn && yarn build`
 3. Run tests
-   `forge test -vvv`
+   `forge test --ffi -vvv`
+
+Check file `test/Counter.t.sol` to see how to mock redstone oracles.
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,14 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
-
-    function setUp() public {
-        counter = new Counter();
-    }
-
+contract MockRedstonePayload is Test {
     function getRedstonePayload(
+        // dataFeedId:value:decimals
         string memory priceFeed
     ) public returns (bytes memory) {
         string[] memory args = new string[](3);
@@ -21,15 +16,22 @@ contract CounterTest is Test {
 
         return vm.ffi(args);
     }
+}
+
+contract CounterTest is Test, MockRedstonePayload {
+    Counter public counter;
+
+    function setUp() public {
+        counter = new Counter();
+    }
 
     function testOracleData() public {
-        bytes memory redstonePayload = getRedstonePayload("BTC:120:8");
+        bytes memory redstonePayload = getRedstonePayload("BTC:120:8,ETH:69:8");
 
         bytes memory encodedFunction = abi.encodeWithSignature(
             "saveOracleData(bytes32)",
             bytes32("BTC")
         );
-
         bytes memory encodedFunctionWithRedstonePayload = abi.encodePacked(
             encodedFunction,
             redstonePayload
